3c54968b1eaeb6d67aafe8c49578c12322bca738
jubaer2222 <139365401+jubaer2222@users.noreply.github.com>
2025-08-19 17:54:02 +0600

    added some css

--- src/components/Bottle/Bottle.jsx
+++ src/components/Bottle/Bottle.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './Bottle.css'
 
 const Bottle = ({bottle}) => {
-    const {img} = bottle;
+    const {img,name,price,stock} = bottle;
     // console.log(bottle);
     return (
-        <div className='card'>
+        <div className='card bottle'>
             <img src={img}></img>
+            <h3>{name}</h3>
+            <p>${price}</p>
+            <p>{stock} remaining</p>
         </div>
     );
 };
--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -1,15 +1,18 @@
 import React, { use } from 'react';
 import Bottle from '../Bottle/Bottle';
+import './Bottles.css';
 
 const Bottles = ({bottlesPromise}) => {
     const bottles = use(bottlesPromise);
     // console.log(bottles);
     return (
-        <div>
+        <div >
             <h3>Bottles : {bottles.length}</h3>
-            {
+          <div className='bottles-container'>
+              {
                 bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle}></Bottle>)
             }
+          </div>
         </div>
     );
 };
